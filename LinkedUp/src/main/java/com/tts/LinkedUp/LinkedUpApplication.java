648db674a1859d9d0ee606373174e3741f78acbc
Walid Asmar <walid.e.asmar@gmail.com>
2018-12-14 15:52:32 -0500

    Adding Project Folder

---
+++ LinkedUp/src/main/java/com/tts/LinkedUp/LinkedUpApplication.java
@@ -0,0 +1,14 @@
+package com.tts.LinkedUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedUpApplication.class, args);
+	}
+
+}
+
