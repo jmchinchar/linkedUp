db728c85460b4fad8c6cab7d79cc738ff1d24ff0
Walid Asmar <walid.e.asmar@gmail.com>
2018-12-16 13:39:59 -0500

    build out user model and controller

--- LinkedUp/src/main/java/com/tts/LinkedUp/User/UserController.java
+++ LinkedUp/src/main/java/com/tts/LinkedUp/User/UserController.java
@@ -1,8 +1,13 @@
 package com.tts.LinkedUp.User;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,5 +21,36 @@ public class UserController {
 		ModelAndView mv = new ModelAndView("user/index");
 		return mv;
 	}
+	
+	@GetMapping("/signup")
+	public ModelAndView newUser(User user) {
+		ModelAndView mv = new ModelAndView("user/newUser");
+		return mv;
+	}
 
+	//handles the saving of the new user
+	@PostMapping("/signup")
+	public ModelAndView createUser(User user) {
+		ModelAndView mv = new ModelAndView("user/index");
+		userRepository.save(user);
+//		mv.addObject("user", user);
+		return mv;
+	}
+	
+	//shows the form for editing a new user
+	@GetMapping("/user/edit/{id}")
+	public ModelAndView updateFormForUser(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("user/editUser");
+		Optional<User> userInfo = userRepository.findById(id);
+		mv.addObject("user", userInfo);
+		return mv;
+	}
+	
+	//saves the edits to the user
+	@PutMapping("/user/edit")
+	public ModelAndView updateUser(User user) {
+		ModelAndView mv = new ModelAndView("redirect:/");
+		userRepository.save(user);
+		return mv;
+	}
 }
